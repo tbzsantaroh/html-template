0ec20b47053d9bb3a08522c799f6d7494bd6996c
tbz <tbz.ofi@gmail.com>
2016-11-08 02:05:10 +0900

    いらないファイル消し忘れてた
    css/base.css 以外全部いらないので消した

--- docs/html-standard01/js/template-header.js
+++
@@ -1,7 +0,0 @@
-function header(){
-    var html = "";
-    html += '<footer>';
-    html += '<p><small>copyright © All Rights Reserved.</small></p>';
-    html += '</footer>';
-    document.write(html);
-}
